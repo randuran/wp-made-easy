d31bdcc4387f9af4f3b7525c8ca0cc65d0dbbfd7
Randy Durán <randy.duran@insimia.com>
2022-01-06 22:07:43 +0100

    initial commit

--- src/traits/WithPage.php
+++ src/traits/WithPage.php
@@ -6,7 +6,33 @@ trait WithPage
 {
     public $pages = [];
 
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function registerPages()
     {
+        add_action('admin_menu', [$this, 'register_menu_pages']);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function register_menu_pages()
+    {
+        foreach ($this->pages as $page) {
+            add_menu_page(
+                $page['title'], // page <title>Title</title>
+                $page['menu_title'], // menu link text
+                $page['capability'], // capability to access the page
+                $page['slug'], // page URL slug
+                $page['callback'], // callback function /w content
+                $page['icon'], // menu icon
+                $page['position'] // priority
+            );
+        }
     }
 }
