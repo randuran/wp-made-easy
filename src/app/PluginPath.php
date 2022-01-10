d365c8946c2c228054f9e2f0a24189f588e2aa26
Randy Durán <randy.duran@insimia.com>
2022-01-10 14:20:28 +0100

    Initial commit

--- src/app/PluginPath.php
+++ src/app/PluginPath.php
@@ -9,7 +9,7 @@ class PluginPath
 
     public function __construct()
     {
-        $this->plugin_path = plugin_dir_path(dirname(__FILE__, 2));
-        $this->plugin_url = plugin_dir_url(dirname(__FILE__, 2));
+        $this->plugin_path = plugin_dir_path(dirname(__FILE__, 5));
+        $this->plugin_url = plugin_dir_url(dirname(__FILE__, 5));
     }
 }
